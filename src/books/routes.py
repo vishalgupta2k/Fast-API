ec7d92f2f0cdc750c398a0ab1116de76c46c22fd
vishalgupta2k <mahajanvishu722@gmail.com>
2025-01-26 19:17:53 +0530

    routes for CRUD

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,41 +1,49 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from fastapi.exceptions import HTTPException
 from typing import List
-from src.books.book_data import books
+# from src.books.book_data import books
 from src.books.schemas import Book, BookUpdate
-
+from src.db.main import get_session
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from src.books.service import BookService
 router = APIRouter()
+book_service = BookService()
 
 @router.get("/", response_model=dict)
-async def get_books(status_code=status.HTTP_200_OK):
+async def get_books(session: AsyncSession = Depends(get_session),status_code=status.HTTP_200_OK):
+    books = await book_service.get_all_books(session)
+    if books is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No books found")
+
     return {"data": books, "status": status_code, "message": "Books retrieved successfully"}
 
-@router.get("/{book_id}", response_model=dict)
-async def get_book(book_id: int, status_code=status.HTTP_200_OK):
-    for book in books:
-        if book["id"] == book_id:
-            return {"data": book, "status": status_code, "message": "Book retrieved successfully"}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
-async def add_book( book_data: Book) -> dict:
-    new_book = book_data.model_dump()
-    books.append(new_book)
+@router.get("/{book_uid}", response_model=dict)
+async def get_book(book_uid: int,session: AsyncSession = Depends(get_session) ,status_code=status.HTTP_200_OK):
+    book =  await book_service.get_book(book_uid, session)
+    if book is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+    return {"data": book, "status": status_code, "message": "Book retrieved successfully"}
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=Book)
+async def add_book( book_data: Book,session: AsyncSession = Depends(get_session)) -> dict:
+    new_book = await book_service.create_book(book_data,session)
     return {"message": "Book added successfully", "status": 201, "data": new_book}
 
-@router.put("/{book_id}")
-async def update_book(book_id: int, book_data: BookUpdate, status_code=status.HTTP_204_NO_CONTENT):
-    new_data = book_data.model_dump()
-    for book in books:
-        if book["id"] == book_id:
-            book.update(new_data)
-            return {"message": "Book updated successfully", "status":status_code ,"data": new_data}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-@router.delete("/{book_id}")
-async def delete_book(book_id: int, status_code=status.HTTP_204_NO_CONTENT):
-    for book in books:
-        if book["id"] == book_id:
-            books.remove(book)
-            return {"message": "Book deleted", "status": 204, "data": book}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+@router.put("/{book_uid}")
+async def update_book(book_uid: int, book_data: BookUpdate, session: AsyncSession = Depends(get_session), status_code=status.HTTP_204_NO_CONTENT):
+    new_data = book_data.model_dump()
+    updated_book = await book_service.update_book(book_uid, new_data, session)
+    if updated_book is not None:
+        return {"message": "Book updated successfully", "status":status_code ,"data": updated_book}
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+@router.delete("/{book_uid}")
+async def delete_book(book_uid: int, session: AsyncSession = Depends(get_session),status_code=status.HTTP_204_NO_CONTENT):
+    book = await book_service.delete_book(book_uid, session)
+    if book is not None:
+        return {"message": "Book deleted", "status": 204, "data": book}
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
