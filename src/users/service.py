44923faab2b95c324abab23d1eebf436cc11f62c
vishalgupta2k <mahajanvishu722@gmail.com>
2025-02-22 17:57:46 +0530

    Modified login user logic to send barrer token while user login

--- src/users/service.py
+++ src/users/service.py
@@ -1,11 +1,13 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.exceptions import HTTPException
-from .schemas import UserCreateModel,UserLogin
+from .schemas import UserCreateModel,UserLogin,LoginUserResponse,LoginResponse
 from sqlmodel import select,desc
 from .models import User
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from .utils import create_access_token
+from src.config import Config
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class UserService:
     async def create_user(self,user_data:UserCreateModel,session:AsyncSession):
@@ -27,8 +29,22 @@ class UserService:
     async def login_user(self, user_data: UserLogin, session: AsyncSession):
         result = await session.execute(select(User).where(User.email == user_data.email))
         user = result.scalars().first()
+        
         if not user:
             raise HTTPException(status_code=400, detail="User not found")
+        
         if not pwd_context.verify(user_data.password, user.password):
             raise HTTPException(status_code=400, detail="Invalid password or email")
-        return user
+        
+        access_token_expires = timedelta(minutes=Config.ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.email}, expires_delta=access_token_expires,
+        )
+        
+        user_response = LoginUserResponse(
+            username=user.username,
+            email=user.email,
+            uid=user.uid,
+        )
+        
+        return LoginResponse(token=access_token, token_type="bearer", user=user_response)
