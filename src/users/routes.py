da7dc97dfba7158611a7ba597d1e1b1a38800828
vishalgupta2k <mahajanvishu722@gmail.com>
2025-02-22 17:31:19 +0530

    updated response model

--- src/users/routes.py
+++ src/users/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, Depends,Form
 from fastapi.exceptions import HTTPException
 from typing import List, Annotated
-from src.users.schemas import User, UserCreateModel, UserResponse,UserLoginResponse,UserLogin
+from src.users.schemas import User, UserCreateModel, UserResponse,UserLogin,LoginResponse
 from src.db.main import get_session
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from src.users.service import UserService
@@ -13,7 +13,7 @@ async def add_user(user_data: UserCreateModel, session: AsyncSession = Depends(g
     new_user = await user_service.create_user(user_data, session)
     new_user_dict = new_user.dict()
     return new_user
-@router.post("/login", status_code=status.HTTP_200_OK, response_model=UserResponse)
+@router.post("/login", status_code=status.HTTP_200_OK, response_model=LoginResponse)
 async def login_user(user_data: Annotated[UserLogin,Form()], session: AsyncSession = Depends(get_session)) -> dict:
     user = await user_service.login_user(user_data, session)
     user_dict = user.dict()
