f1d41e0dd74774e7464cf97983e72399cc56ec8f
vishalgupta2k <mahajanvishu722@gmail.com>
2025-02-22 17:53:35 +0530

    Added utils file for users

---
+++ src/users/utils.py
@@ -0,0 +1,14 @@
+from src.config import Config
+from datetime import datetime, timedelta, timezone
+import jwt
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, Config.SECRET_KEY, algorithm= Config.ALGORITHM)
+    return encoded_jwt
