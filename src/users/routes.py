3956713c428a917e9a2afad5c3f0a520e4beedbe
vishalgupta2k <mahajanvishu722@gmail.com>
2025-02-22 12:59:09 +0530

    handle form data

--- src/users/routes.py
+++ src/users/routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends,Form
 from fastapi.exceptions import HTTPException
-from typing import List
+from typing import List, Annotated
 from src.users.schemas import User, UserCreateModel, UserResponse,UserLoginResponse,UserLogin
 from src.db.main import get_session
 from sqlalchemy.ext.asyncio.session import AsyncSession
@@ -14,7 +14,7 @@ async def add_user(user_data: UserCreateModel, session: AsyncSession = Depends(g
     new_user_dict = new_user.dict()
     return new_user
 @router.post("/login", status_code=status.HTTP_200_OK, response_model=UserResponse)
-async def login_user(user_data: UserLogin, session: AsyncSession = Depends(get_session)) -> dict:
+async def login_user(user_data: Annotated[UserLogin,Form()], session: AsyncSession = Depends(get_session)) -> dict:
     user = await user_service.login_user(user_data, session)
     user_dict = user.dict()
     return user_dict
